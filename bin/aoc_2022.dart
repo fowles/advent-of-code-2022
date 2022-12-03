60e4bcca79e630edcb7cf99b3443f8e29811cde1
Matt Kulukundis <matt.fowles@gmail.com>
2022-12-02 22:43:01 -0500

    Implement day1 along with a basic driver

--- bin/aoc_2022.dart
+++ bin/aoc_2022.dart
@@ -1,5 +1,13 @@
-import 'package:aoc_2022/day01.dart' as day;
+import 'package:format/format.dart';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${day.calculate()}!');
+import 'package:aoc_2022/day01.dart' as day01;
+
+void main(List<String> args) async {
+  var days = [ day01.solve ];
+  var pos = args.length > 0 ? int.parse(args[0]) : days.length - 1;
+  var file = "problems/day{:02d}.txt".format(pos + 1);
+  var p = await File(file).readAsString();
+
+  days[pos](p.trim());
 }
--- lib/day01.dart
+++ lib/day01.dart
@@ -1,3 +1,24 @@
-int calculate() {
-  return 6 * 7;
+int sum(int a, int b) {
+  return a + b;
 }
+
+void solve(String raw) {
+  List<List<int>> elves = raw
+      .split("\n\n")
+      .map(
+        (elf) => elf.split("\n").map(int.parse).toList()
+      ).toList();
+  var s = elves.map((elf) => elf.reduce(sum)).toList();
+  s.sort();
+  print(part1(s));
+  print(part2(s));
+}
+
+int part1(List<int> elves) {
+  return elves.last;
+}
+
+int part2(List<int> elves) {
+  return elves.reversed.take(3).reduce(sum);
+}
+
