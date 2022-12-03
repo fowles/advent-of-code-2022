2c38a99d2102b1062c95ba6000906e70025ba222
Matt Kulukundis <matt.fowles@gmail.com>
2022-12-02 21:17:05 -0500

    initial version

---
+++ bin/aoc_2022.dart
@@ -0,0 +1,5 @@
+import 'package:aoc_2022/day01.dart' as day;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day.calculate()}!');
+}
---
+++ test/aoc_2022_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc_2022/aoc_2022.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
